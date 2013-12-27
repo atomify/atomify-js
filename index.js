4ec6e8742833b5bf4d6de9409b5a19c8963b54c5
Ben Ng <me@benng.me>
2013-12-26 18:14:26 -0800

    Add envify

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var browserify = require('browserify')
   , shim       = require('browserify-shim')
+  , envify     = require('envify')
   , hbsfy      = require('hbsfy')
   , brfs       = require('brfs')
 
@@ -7,6 +8,7 @@ module.exports = function (opts, cb) {
   if (!opts.shim) opts.shim = {}
   var bundle = shim(browserify(), opts.shim)
   bundle.require(require.resolve(opts.entry), {entry: true})
+  bundle.transform(envify)
   bundle.transform(hbsfy)
   bundle.transform(brfs)
   bundle.bundle({debug: opts.debug || false}, cb)
