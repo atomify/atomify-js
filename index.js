13a04629668b76e7b2d60de816e6be46fb7086f3
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-01-25 13:49:10 -0500

    Make brfs run after all other transforms

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ var browserify = require('browserify')
 module.exports = function (opts, cb) {
   if (!opts.shim) opts.shim = {}
   var bundle = shim(browserify(), opts.shim)
+
   bundle.require(require.resolve(opts.entry), {entry: true})
-  opts.transforms = [envify, hbsfy, brfs].concat(opts.transforms || [])
+
+  // ensure brfs runs last because it requires valid js
+  opts.transforms = [envify, hbsfy].concat(opts.transforms || []).concat(brfs)
   opts.transforms.forEach(function (transform) {
     bundle.transform(transform)
   })
+
   bundle.bundle({debug: opts.debug || false}, cb)
 }
