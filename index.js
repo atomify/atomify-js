bd96d41f8842fa84d340f553a36c17daa91479ba
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-02-14 23:59:58 -0500

    Rename var because bundle.bundle() is confusing

--- index.js
+++ index.js
@@ -16,25 +16,25 @@ module.exports = function (opts, cb) {
   opts = opts || {}
   opts.shim = opts.shim || {}
   opts.debug = opts.debug || false
-  var bundle = shim(opts.watch ? watchify() : browserify(), opts.shim)
+  var b = shim(opts.watch ? watchify() : browserify(), opts.shim)
 
   if (opts.watch) {
-    bundle.on('update', function () {
-      bundle.bundle(opts, cb)
+    b.on('update', function () {
+      b.bundle(opts, cb)
     })
   }
 
-  bundle.require(path.resolve(process.cwd(), opts.entry), {entry: true})
+  b.require(path.resolve(process.cwd(), opts.entry), {entry: true})
 
   // ensure brfs runs last because it requires valid js
   opts.transforms = [envify, ejsify, hbsfy, jadeify, partialify].concat(opts.transforms || []).concat(brfs)
   opts.transforms.forEach(function (transform) {
-    bundle.transform(transform)
+    b.transform(transform)
   })
 
   if (opts.output) {
-    bundle.bundle(opts, writer(path.resolve(process.cwd(), opts.output), {debug: opts.debug}))
+    b.bundle(opts, writer(path.resolve(process.cwd(), opts.output), {debug: opts.debug}))
   } else {
-    bundle.bundle(opts, cb)
+    b.bundle(opts, cb)
   }
 }
