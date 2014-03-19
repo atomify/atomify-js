29a086f8eb456d79b92ed66b006b6365ec6005ce
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-03-18 22:09:43 -0400

    Test assets config option

---
+++ test/assets.js
@@ -0,0 +1,22 @@
+var test = require('tape')
+  , js = require('../')
+  , fs = require('fs')
+  , prefix = __dirname + '/fixtures/assets/'
+  , read = function (file) {
+    return fs.readFileSync(prefix + file, 'utf8')
+  }
+
+test('opts.assets', function (t) {
+  t.plan(2)
+
+  js({
+    entry: prefix + 'entry.js'
+    , assets: {
+      dest: prefix + '/output/img'
+      , prefix: 'img/'
+    }
+  }, function (err, src) {
+    t.equal(read('output/bundle.js'), src)
+    t.ok(fs.existsSync(prefix + '/output/img/4314d804f81c8510.png'), 'file exists')
+  })
+})
---
+++ test/fixtures/assets/entry.js
@@ -0,0 +1,1 @@
+var html = require('./entry.html')
