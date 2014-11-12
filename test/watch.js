f044c40fe368c59b1f3e305a86dd2a15dafd6623
Joey Baker <joey@byjoeybaker.com>
2014-11-11 20:47:43 -0800

    Add opts.watch test
    because the upgrade to watchify broke things.

---
+++ test/watch.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var test = require('tape')
+  , lib = require('../index.js')
+  , path = require('path')
+  , fs = require('fs')
+  , entryPath = path.join(__dirname, 'fixtures', 'watch')
+  , outputPath = path.join(entryPath, 'output')
+  , changerPath = path.join(outputPath, 'changer.js')
+  , now = Date.now()
+  , mkdirp = require('mkdirp')
+  , setup = function setup(){
+    var file = 'module.exports = ' + now
+
+    mkdirp.sync(outputPath)
+    fs.writeFileSync(changerPath, file)
+  }
+
+test('opts.watch', function(t){
+  var callbackCallCount = 0
+    , callback
+    , w
+
+  setup()
+  t.plan(4)
+
+  // get the watchify instance so that we can close it an end the test
+  lib.emitter.once('watchify', function assignWatchify(watchify){
+    w = watchify
+  })
+
+  // callback will be called each time the changer file is changed
+  callback = function callback(err, bundle){
+    var src = bundle.toString()
+      , changerContents2 = 'module.exports = changed'
+
+    callbackCallCount++
+
+    // first call only
+    if (callbackCallCount === 1){
+      t.error(err, 'does not error on initial callback')
+
+      t.ok(
+        src.indexOf(now) > -1
+        , 'contains the watched dep on inital callback'
+      )
+
+      // commit a change to the file so that we trigger the callback again
+      fs.writeFileSync(changerPath, changerContents2)
+    }
+    else {
+      t.error(err, 'does not error on the second callback')
+
+      t.ok(
+        src.indexOf(changerContents2) > -1
+        , 'contains the watched dep on the second callback'
+      )
+
+      // close all file handlers ← important so that tests exit
+      w.close()
+    }
+  }
+
+  // run the lib with watchify enabled
+  lib({watch: true, entry: path.join(entryPath, 'index.js')}, callback)
+})
