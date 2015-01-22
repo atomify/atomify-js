10bfff92326502ea2a2c47751412412ed84bff0d
Joey Baker <joey@byjoeybaker.com>
2015-01-22 11:28:58 -0800

    Upgrade minifify
    Now outputs buffers like it’s supposed to.

--- test/minify.js
+++ test/minify.js
@@ -9,7 +9,7 @@ test('minifies code', function (t) {
   js({entries: [path.join(prefix, '/entry.js')], minify: {map: 'app.map.json'}}, function (err, src, map) {
     t.error(err, 'should not error')
     t.ok(map, 'there should be a map')
-    t.ok(src.indexOf('should keep') > -1, 'is correct')
-    t.ok(src.indexOf('should not keep') < 0, 'removed dead code')
+    t.ok(src.toString().indexOf('should keep') > -1, 'is correct')
+    t.ok(src.toString().indexOf('should not keep') < 0, 'removed dead code')
   })
 })
